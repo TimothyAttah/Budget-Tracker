519a7ef3bfa9dd84654cbee1922f477fdf37de5c
Timothy Attah <timothyattah733@gmail.com>
2021-03-17 22:16:02 +0100

    changed files according to eslint

--- client/src/pages/budget/Balance.jsx
+++ client/src/pages/budget/Balance.jsx
@@ -2,16 +2,13 @@ import React, { useContext } from 'react';
 import { GlobalContext } from '../../context/BudgetContext';
 
 const Balance = () => {
-  const { incomes, expenses } = useContext( GlobalContext )
-  
-  const incomeTransaction = incomes.map(income=> income.value)
-  const expenseTransaction = expenses.map( expense => expense.value )
-  
-  const totalIncome = incomeTransaction.reduce( ( result, item ) => ( result += item ), 0 ).toFixed( 2 );
-  
-  const totalExpenses = expenseTransaction.reduce( ( result, item ) => ( result += item ), 0 ).toFixed( 2);
+  const { incomes, expenses } = useContext( GlobalContext );
 
-  const totalBalance = (totalIncome - totalExpenses).toFixed(2)
+  const incomeTransaction = incomes.map( income => income.value );
+  const expenseTransaction = expenses.map( expense => expense.value );
+  const totalIncome = incomeTransaction.reduce( ( result, item ) => ( result += item ), 0 ).toFixed( 2 );
+  const totalExpenses = expenseTransaction.reduce( ( result, item ) => ( result += item ), 0 ).toFixed( 2 );
+  const totalBalance = ( totalIncome - totalExpenses ).toFixed( 2 );
   return (
     <div>
       <h2>Your Balance</h2>
@@ -23,7 +20,7 @@ const Balance = () => {
         <p>-${ totalExpenses }</p>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Balance;
